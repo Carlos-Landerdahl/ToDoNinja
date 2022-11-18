8a89aced4c57540910fea8171c1d538c8610ea8a
charlim <carloslanderdahl27@gmail.com>
2022-11-18 01:19:17 -0300

    Starting repository

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Header } from "./components/Header"
+import { TaskList } from "./components/TaskList"
+
+// import styles from './App.module.css';
+import './global.css'
+
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <TaskList />
+    </div>
+  )
+}
+
+export default App
